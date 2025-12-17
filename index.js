0e47b5fdbbd75500280ca83e3eceddc39833665c
Vittoria <vitto.tonini@gmail.com>
2025-12-17 19:32:09 +0100

    added GET by id

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express"); //importiamo il modulo express installato tramite npm e lo assegniamo alla variabile
 const app = express(); //la funzione express viene utilizzata per creare un'istanza dell'applicazione Express
-const {persone} = require("./persone"); //dall’oggetto esportato, prendiamo solo la proprietà persone e la mettiamo nella variabile persone
+const { persone } = require("./persone"); //dall’oggetto esportato, prendiamo solo la proprietà persone e la mettiamo nella variabile persone
 
 //GET
-app.get("/api/persone", (req, res)=>{
-  res.status(200).json({data: persone});
+app.get("/api/persone", (req, res) => {
+  res.status(200).json({ success: true, data: persone });
 })
+
+//GET by id
+app.get("/api/persone/:id", (req, res) =>{
+  const {id} = req.params; //req.params è un oggetto che contiene tutti i parametri della route dinamica, prendiamo la prop id dell'oggetto e la mettiamo nella variabile id
+  
+  const persona = persone.find(
+    (persona) => persona.id === id
+  );
+
+  res.json({success: true, data: persona});
+})
+
+
 
 app.listen(3000)
