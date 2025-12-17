6171cb0b2e1984b81f3eacdce8c0d9e5bd23de2b
Vittoria <vitto.tonini@gmail.com>
2025-12-17 20:53:16 +0100

    modified html body

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); //la funzione express viene utilizzata per creare un'ista
 const { persone } = require("./persone"); //dall'oggetto esportato, prendiamo solo la proprietà persone e la mettiamo nella variabile persone
 
 app.use(express.json()); //questo middleware serve per fare POST da postman
-app.use(express.urlencoded({extended: false})); //questo middleware serve per leggere i dati da un form
+app.use(express.urlencoded({ extended: false })); //questo middleware serve per leggere i dati da un form
 
 //GET: visualizzare tutte le persone
 app.get("/api/persone", (req, res) => {
@@ -11,14 +11,14 @@ app.get("/api/persone", (req, res) => {
 })
 
 //GET by id: visualizzare una persona specifica
-app.get("/api/persone/:id", (req, res) =>{
-  const {id} = req.params; //req.params è un oggetto che contiene tutti i parametri della route dinamica, prendiamo la prop id dell'oggetto e la mettiamo nella variabile id
-  
+app.get("/api/persone/:id", (req, res) => {
+  const { id } = req.params; //req.params è un oggetto che contiene tutti i parametri della route dinamica, prendiamo la prop id dell'oggetto e la mettiamo nella variabile id
+
   const persona = persone.find(
     (persona) => persona.id === id
   );
 
-  res.json({success: true, data: persona});
+  res.json({ success: true, data: persona });
 })
 
 //POST: inserire una persona
@@ -26,7 +26,7 @@ app.post("/api/persone", (req, res) => {
   console.log(req.body);
   const persona = req.body;
   persone.push(persona);
-  res.status(200).json({success: true, data: persone});
+  res.status(200).json({ success: true, data: persone });
 })
 
 app.listen(3000);
