5610455f0eb95180efb1ede8c9098f6e98c4bd0e
Vittoria <vitto.tonini@gmail.com>
2025-12-17 22:20:21 +0100

    added PUT API

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express"); //importiamo il modulo express installato tramite npm e lo assegniamo alla variabile
 const app = express(); //la funzione express viene utilizzata per creare un'istanza dell'applicazione Express
 const { persone } = require("./persone"); //dall'oggetto esportato, prendiamo solo la proprietà persone e la mettiamo nella variabile persone
+const path = require("path");
 
+app.use(express.static(path.join(__dirname))); //serve per rendere index.html accessibile su http://localhost:3000
 app.use(express.json()); //questo middleware serve per fare POST da postman
 app.use(express.urlencoded({ extended: false })); //questo middleware serve per leggere i dati da un form
 
@@ -21,12 +23,21 @@ app.get("/api/persone/:id", (req, res) => {
   res.json({ success: true, data: persona });
 })
 
-//POST: inserire una persona
+//POST: inserire una persona (da form compilato dall'utente)
 app.post("/api/persone", (req, res) => {
   console.log(req.body);
   const persona = req.body;
   persone.push(persona);
   res.status(200).json({ success: true, data: persone });
+})
+
+//PUT: modificare una persona
+app.put("api/persone/:id", (res, req) =>{
+  const{id} = req.params;
+  const persona = req.body;
+  persone[Number(id)-1] = persona;
+  res.status(200).json({success: true, data: persone});
+  
 })
 
 app.listen(3000);
